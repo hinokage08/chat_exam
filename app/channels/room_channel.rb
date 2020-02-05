64bfe71ad584e0323fffc16dd69561aafb0ca355
youichi yamazaki <denncoru@yahoo.co.jp>
2020-02-05 16:50:36 +0900

    new app

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,21 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    message = Message.create!(content: data['message'],
+                              user_id: current_user.id,
+                              is_read: false,
+                              room_id: params['room_id'])
+    render_message = ApplicationController.renderer.render(partial: 'messages/message',
+                                                            locals: {message: message})
+    ActionCable.server.broadcast 'room_channel',
+                                  message: render_message,
+                                  room_id: params['room_id']
+  end
+end
